3e88159ec415c23f48628c652b24d1f00b5c2654
Bhavesh <mahajanbhaveshop2@gmail.com>
2023-07-20 11:24:57 +0530

    my commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function ChatStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator defaultScreenOptions={Home}>
+      <Stack.Screen name='Home' component={Home} />
+      <Stack.Screen name='Chat' component={Chat} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function RootNavigator() {
+//   const { user, setUser } = useContext(AuthenticatedUserContext);
+//   const [isLoading, setIsLoading] = useState(true);
+// useEffect(() => {
+//     const unsubscribeAuth = onAuthStateChanged(
+//       auth,
+//       async authenticatedUser => {
+//         authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+//         setIsLoading(false);
+//       }
+//     );
+//     return unsubscribeAuth;
+//   }, [user]);
+// if (isLoading) {
+//     return (
+//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//         <ActivityIndicator size='large' />
+//       </View>
+//     );
+//   }
+
+return (
+    <NavigationContainer>
+      {/* {user ? <ChatStack /> : <AuthStack />} */}
+      <ChatStack/>
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return  <RootNavigator />
+}
+
+
+
+
